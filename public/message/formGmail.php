2a750ccbb88aabd4edee7d9300342b7d1b4fbe76
Aranda <antonystatus.com>
2024-03-01 09:55:42 -0500

    Otro cambio fase 2

--- public/message/formGmail.php
+++ public/message/formGmail.php
@@ -176,7 +176,7 @@ function SendMessage($menssage,$imagenes_main,$title){
         $asunto = $title;
         $msg = $mensages_main;
     
-        $header = "From: noreply@example.com\r\n";
+        $header = "From: noreply@example.com\r\n";//pones aqui el correo del remitente
         $header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
         $header .= "Mime-Version: 1.0 \r\n";
         $header .= "Content-Type: text/html";
