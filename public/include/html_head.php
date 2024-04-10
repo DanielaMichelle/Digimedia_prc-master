6e1c6395f1cd2e177171cf8a7e8599169a4a961a
XxEduardo032xX <153869902+XxEduardo032xX@users.noreply.github.com>
2024-04-10 11:06:40 -0500

    Modificacion de las urls y google

--- public/include/html_head.php
+++ public/include/html_head.php
@@ -41,6 +41,17 @@
      <!-- JS NUBE -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     
+    <!-- GOGGLE ANALITYCS -->
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'UA-143365030-1');
+    </script>
     
     
     <!-- js local -->
