5adc13d00cf49cd6110ccb0fca617c44678ff864
Diego Levano <diegolevanososa@gmail.com>
2024-04-29 15:05:02 -0500

    test cambios img

--- public/view/index.php
+++ public/view/index.php
@@ -2,7 +2,8 @@
 <html lang="en">
 <?php
 //I'm the God Dog
-$fileCSS    = ['index', 'style', 'inicio-minified', 'header', 'footer', 'section-minified'];
+//Sin bootstrap - mejora rendimiento performans
+$fileCSS    = ['index', 'style', 'inicio-minified', 'header', 'footer', 'section-minified', 'bootstrap'];
 include_once './public/include/html_head.php';
 ?>
 
@@ -12,16 +13,11 @@ include_once './public/include/html_head.php';
         height: 100vh;
         position: relative;
         overflow: hidden;
-/*         background: -webkit-linear-gradient(to right,
-                rgb(167 41 189 / 62%),
-                rgba(170, 43, 193, 0.62),
-                rgba(249, 54, 204, 0.5)),
-            url("./public/img/hero-image3.webp");
         background: linear-gradient(to right,
                 rgb(167 41 189 / 62%),
                 rgba(170, 43, 193, 0.62),
                 rgba(249, 54, 204, 0.5)),
-            url("./public/img/hero-image3.webp"); */
+            url("./public/img/hero-image3.webp");
         background-repeat: no-repeat;
         font-family: "Poppins", sans-serif;
         font-style: normal;
@@ -33,16 +29,11 @@ include_once './public/include/html_head.php';
     @media screen and (max-width: 426px) {
         .hero {
             height: 85vh;
-/*             background: -webkit-linear-gradient(to right,
-                    rgb(167 41 189 / 62%),
-                    rgba(170, 43, 193, 0.62),
-                    rgba(249, 54, 204, 0.5)),
-                url("./public/img/hero-image3-mobile-.webp");
             background: linear-gradient(to right,
                     rgb(167 41 189 / 62%),
                     rgba(170, 43, 193, 0.62),
                     rgba(249, 54, 204, 0.5)),
-                url("./public/img/hero-image3-mobile.webp"); */
+                url("./public/img/hero-image3-mobile.webp");
             background-repeat: no-repeat;
             background-position: center;
             background-size: cover;
@@ -52,16 +43,11 @@ include_once './public/include/html_head.php';
 
     @media screen and (max-width: 768px) {
         .hero {
-/*             background: -webkit-linear-gradient(to right,
-                    rgb(167 41 189 / 62%),
-                    rgba(170, 43, 193, 0.62),
-                    rgba(249, 54, 204, 0.5)),
-                url("./public/img/hero-image3-tablet.webp");
             background: linear-gradient(to right,
                     rgb(167 41 189 / 62%),
                     rgba(170, 43, 193, 0.62),
                     rgba(249, 54, 204, 0.5)),
-                url("./public/img/hero-image3-tablet.webp"); */
+                url("./public/img/hero-image3-tablet.webp"); 
             background-repeat: no-repeat;
             background-position: center;
             background-position-y: 4rem;
